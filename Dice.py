f65b39ae5c0a8e4b7af7db378b6c34ef90456bc4
GODWIN USHIE <143878457+Godwinushie619@users.noreply.github.com>
2024-03-02 14:45:14 +0100

    Add files via upload

---
+++ Dice.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+
+
+
+
+st.set_page_config(page_title="Dice game", page_icon="🎲🎲🎲🎲", layout="wide")
+st.sidebar.image("dice_animation.gif",width=1000)
+
+if st.button
+
+def dice_game():
+    a, b = np.random.randint(1,7,2)
+    st.write(f"Your numbers are {a} and {b}")
+    if a==6 and b ==6:
+        st.balloons()
+        return('Congratulations!!!!!!! You don chop 👏')
+    else:
+        return("Try again, maybe you go chop next time🤣😂🤣😂")
+
+
+if st.button("Play"):
+    st.write(dice_game())
